057c8df711fd1208670a2465f9789c981d95c61e
jasonmarkbeaton <jason.beaton@gmail.com>
2015-10-19 16:50:20 -0400

    Setup a domain helper on the postItem.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,17 +1,17 @@
 var postsData = [
-	{
-		title: 'Introducing Telescope',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com'
-	}
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  }, 
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  }, 
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
 ];
 Template.postsList.helpers({
-	posts: postsData
+  posts: postsData
 });
